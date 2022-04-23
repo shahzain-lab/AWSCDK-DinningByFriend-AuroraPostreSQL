d7f746ed093025c1e1fa833fb70fe61d33525a5c
Syed ZainUlAbideen <shahzain30196@gmail.com>
2022-04-23 12:22:04 +0500

    Initial commit

---
+++ test/awscdk-dinning_by_friend-aurora_postre_sql.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwscdkDinningByFriendAuroraPostreSql from '../lib/awscdk-dinning_by_friend-aurora_postre_sql-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/awscdk-dinning_by_friend-aurora_postre_sql-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwscdkDinningByFriendAuroraPostreSql.AwscdkDinningByFriendAuroraPostreSqlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
